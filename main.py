67758e3720d3dbcd5a5201a02d3b3a6ef8e573b1
LikeKod <i.serebryanoy@yandex.ru>
2023-11-10 23:44:39 +0300

    Funny project

---
+++ main.py
@@ -0,0 +1,34 @@
+from rembg import remove
+from PIL import Image
+from pathlib import Path
+
+def remove_bg():
+    list_of = ['*.png', '*.jpeg']
+    all_files = []
+
+    for ext in list_of:
+        all_files.extend(Path('redactor_fona\photo').glob(ext))
+
+    # print(all_files)
+
+    for index, item in enumerate(all_files):
+        input_path = Path(item)
+        file_name = input_path.stem
+
+        output_path = f'redactor_fona\photo_new/{file_name}_output.png'
+
+        input_img = Image.open(input_path)
+        output_img = remove(input_img)
+        output_img.save(output_path)
+
+        print(f'Completed: {index+1}/{len(all_files)}')
+
+
+
+def main():
+    remove_bg()
+
+
+
+if __name__ == '__main__':
+    main()
